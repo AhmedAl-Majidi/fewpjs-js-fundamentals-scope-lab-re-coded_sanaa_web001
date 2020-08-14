2865c094235ffa9e0d19b4fdb420681e349e7d0e
Ahmed Al-Majidi <af774857999@gmail.com>
2020-08-14 14:48:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  let animal = 'cat';
   return animal
 }
 
@@ -18,3 +19,4 @@ function add2(n) {
   two = 2
 }
 myAnimal();
+yourAnimal();
